ccebeb23c2c71802c8894674f1765c50761f2f90
osni junior <osniandrade@gmail.com>
2020-07-21 16:59:02 -0300

    working code markes as comentary bellow every change

--- libutil.c
+++ libutil.c
@@ -6,7 +6,7 @@
 /*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 21:37:55 by ocarlos-          #+#    #+#             */
-/*   Updated: 2020/07/21 16:26:03 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2020/07/21 16:58:32 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,8 @@ t_prtform	*ft_initform(t_prtform *form)
 	return (form);
 }
 
-int			ft_countdig(unsigned long long int n, int b) //**********mudar aqui
+int			ft_countdig(unsigned long long int n, int b)
+//int			ft_countdig(unsigned int n, int b)
 {
 	int	i;
 
@@ -56,7 +57,8 @@ void		ft_revstr(char *s, int size)
 	}
 }
 
-char		*ft_itoabase(unsigned long long int n, int b) //********mudar aqui
+char		*ft_itoabase(unsigned long long int n, int b)
+//char		*ft_itoabase(unsigned int n, int b)
 {
 	int		i;
 	int		r;
