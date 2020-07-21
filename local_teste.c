4a82cb83336d368210fe154626e741a7ffc54263
osni junior <osniandrade@gmail.com>
2020-07-21 16:51:12 -0300

    changes to local_teste

--- local_teste.c
+++ local_teste.c
@@ -31,23 +31,41 @@ char	*itoa(unsigned long int n, char type)
 
 int		main(void)
 {
-// 	int     a = -4;
+	int     a = -4;
+    int     b = 0;
+    char    c = 'a';
+    int     d = 2147483647;
+    int     e = -2147483648;
+    int     f = 42;
+    int     g = 25;
+    int     h = 4200;
+    int     i = 8;
+    int     j = -12;
+    int     k = 123456789;
+    int     l = 0;
+    int     m = -12345678;
+    char    *n = "abcdefghijklmnop";
+    char    *o = "-a";
+    char    *p = "-12";
+	char    *q = "0";
+	char    *r = "%%";
+	char    *s = "-2147483648";
+	char    *t = "0x12345678";
+	char    *u = "-0";
+
+	ft_printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j);
+	printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j);
 	
-// 	ft_printf("%*p, %*x, %*p, %*x, %*p, %*x, %*p, %*x\n", a, (void *)209590960, a, 209590960, a, (void *)207038912, a, 207038912, a, (void *)1
-// , a, 1, a, NULL, a, 0);
-// 	printf("%*p, %*x, %*p, %*x, %*p, %*x, %*p, %*x\n", a, (void *)209590960, a, 209590960, a, (void *)207038912, a, 207038912, a, (void *)1
-// , a, 1, a, NULL, a, 0);
+	// unsigned long long int 	a = 93940956356327;
+	// char	*var = "5";
+	// char	*result;
 	
-	unsigned long long int 	a = 93940956356327;
-	char	*var = "5";
-	char	*result;
-	
-	//printf("<%p %x %X>\n", var, a, a);
-	ft_printf("<%p %x %X>\n", var, a, a);
-	result = itoa(a,'x');
-    printf("itoa = %s\n", result);
-    result = ft_itoabase(a, 16);
-	printf("itoabase = %s\n", result);
+	// //printf("<%p %x %X>\n", var, a, a);
+	// ft_printf("<%p %x %X>\n", var, a, a);
+	// result = itoa(a,'x');
+    // printf("itoa = %s\n", result);
+    // result = ft_itoabase(a, 16);
+	// printf("itoabase = %s\n", result);
 
 	return (0);
 }
