d223265b5a1a09865b6ea452bed29e208c800fb6
osni junior <osniandrade@gmail.com>
2020-07-30 11:16:53 -0300

    changed local_teste

--- local_teste.c
+++ local_teste.c
@@ -31,30 +31,30 @@ char	*itoa(unsigned long int n, char type)
 
 int		main(void)
 {
-	// int     a = -4;
-    // int     b = 0;
-    // char    c = 'a';
-    // int     d = 2147483647;
-    // int     e = -2147483648;
-    // int     f = 42;
-    // int     g = 25;
-    // int     h = 4200;
-    // int     i = 8;
-    // int     j = -12;
-    // int     k = 123456789;
-    // int     l = 0;
-    // int     m = -12345678;
-    // char    *n = "abcdefghijklmnop";
-    // char    *o = "-a";
-    // char    *p = "-12";
-	// char    *q = "0";
-	// char    *r = "%%";
-	// char    *s = "-2147483648";
-	// char    *t = "0x12345678";
-	// char    *u = "-0";
+	int     a = -4;
+    int     b = 0;
+    char    c = 'a';
+    int     d = 2147483647;
+    int     e = -2147483648;
+    int     f = 42;
+    int     g = 25;
+    int     h = 4200;
+    int     i = 8;
+    int     j = -12;
+    int     k = 123456789;
+    int     l = 0;
+    int     m = -12345678;
+    char    *n = "abcdefghijklmnop";
+    char    *o = "-a";
+    char    *p = "-12";
+	char    *q = "0";
+	char    *r = "%%";
+	char    *s = "-2147483648";
+	char    *t = "0x12345678";
+	char    *u = "-0";
 
-	// ft_printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j);
-	// printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j);
+	ft_printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j);
+	printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j);
 	
 	//-------------------------------------------------------------------------------
 
@@ -81,9 +81,14 @@ int		main(void)
 	//-------------------------------------------------------------------------------
 
 	int a01 = 20;
+	//int *p01 = a01;
+	unsigned int a02 = 99999;
 
 	printf("%p\n", &a01);
 	ft_printf("%p\n", &a01);
+
+	printf("%u %x %X %u %x %X\n", a02, a02, a02, a02, a02, a02);
+	ft_printf("%u %x %X %u %x %X\n", a02, a02, a02, a02, a02, a02);
 
 	return (0);
 }
